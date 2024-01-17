8f090ea6001233d1bc7db651830ef3827f8dd0aa
William O'Brien <wgo2104@columbia.edu>
2024-01-17 01:39:09 -0500

    event images

--- events/gallery/event_data.js
+++ events/gallery/event_data.js
@@ -1,17 +1,85 @@
+import { eventData } from '../load_event.js';
+
 function loadEvent() {
     const urlParams = new URLSearchParams(window.location.search);
     const eventId = urlParams.get('event'); // Assuming URL is like event_template.html?event=event1
     const event = eventData[eventId];
+    console.log(event);
 
-    if (event) {
-        document.getElementById('event-title').textContent = event.title;
-        const gallery = document.getElementById('event-gallery');
+    if (event.archive == true) {
+        const gallery = document.querySelector('.flyer-gallery');
+        const h2g = document.createElement('h2');
+        h2g.textContent = event.title;
+        gallery.appendChild(h2g); 
         event.images.forEach(imageSrc => {
             const img = document.createElement('img');
             img.src = imageSrc;
             gallery.appendChild(img);
         });
     }
+    else if (event.archive==false) {
+        const gallery = document.querySelector('.flyer-gallery');
+        const h2g = document.createElement('h2');
+        h2g.textContent = event.title;
+        gallery.appendChild(h2g); 
+        const img = document.createElement('img');
+        img.src = '../' + event.flyerSrc;
+        gallery.appendChild(img);
+    }
 }
 
 document.addEventListener('DOMContentLoaded', loadEvent);
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const upcoming_gallery = document.querySelector('.flyer-gallery');
+//     const h2u = document.createElement('h2');
+//     h2u.textContent = 'UPCOMING';
+//     upcoming_gallery.appendChild(h2u);
+//     let upcoming_count=0;
+//     Object.keys(eventData).forEach(key => {
+//         const event = eventData[key];
+//         if (event.archive == false) {
+//             upcoming_count++;
+//             // Create the anchor tag
+//             const a = document.createElement('a');
+//             a.href = `gallery/?event=${event.title}`;
+
+//             // Create the image tag
+//             const img = document.createElement('img');
+//             img.src = event.flyerSrc;
+//             img.alt = event.title;
+
+//             // Append the image to the link, and the link to the container
+//             a.appendChild(img);
+//             upcoming_gallery.appendChild(a);
+//         }
+//     });
+
+//     if (upcoming_count == 0) {
+//         const h3u = document.createElement('h3');
+//         h3u.textContent = 'Loading...';
+//         upcoming_gallery.appendChild(h3u);
+//     }
+
+//     const archive_gallery = document.querySelector('.flyer-gallery');
+//     const h2a = document.createElement('h2');
+//     h2a.textContent = 'ARCHIVE';
+//     archive_gallery.appendChild(h2a);
+//     Object.keys(eventData).forEach(key => {
+//         const event = eventData[key];
+//         if (event.archive == true) {
+//             // Create the anchor tag
+//             const a = document.createElement('a');
+//             a.href = `gallery/?event=${event.id}`;
+
+//             // Create the image tag
+//             const img = document.createElement('img');
+//             img.src = event.flyerSrc;
+//             img.alt = event.title;
+
+//             // Append the image to the link, and the link to the container
+//             a.appendChild(img);
+//             archive_gallery.appendChild(a);
+//         }
+//     });
+// });
--- events/load_event.js
+++ events/load_event.js
@@ -1,59 +1,78 @@
 export const eventData = {
     "ludlow": {
         archive: true,
-        title: "Ludlow Event",
+        title: "Sound Session",
         id: "ludlow",
         flyerSrc: "flyers/ludlow_house.jpeg",
         images: [
-            "gallery/ludlow/000014720010.jpg",
-            "gallery/ludlow/000014720016.jpg",
+            "ludlow/1.jpg",
+            "ludlow/2.jpg",
+            "ludlow/3.jpg",
+            "ludlow/4.jpg",
+            "ludlow/5.jpg",
+            "ludlow/6.jpg",
+            "ludlow/7.jpg", 
+            "ludlow/8.jpg", 
+            "ludlow/9.jpg", 
+            "ludlow/10.jpg",
+            "ludlow/11.jpg", 
+            "ludlow/12.jpg", 
+            "ludlow/13.jpg", 
             // More images
         ]
     },
-    "ludlow": {
+    "its_too_loud": {
         archive: true,
-        title: "Ludlow Event",
-        id: "ludlow",
-        flyerSrc: "flyers/ludlow_house.jpeg",
+        title: "It's Too Loud!",
+        id: "its_too_loud",
+        flyerSrc: "flyers/its_too_loud.jpeg",
         images: [
-            "gallery/ludlow/000014720010.jpg",
-            "gallery/ludlow/000014720016.jpg",
+            "too_loud/1.jpg",
+            "too_loud/2.jpg",
+            "too_loud/3.jpg",
+            "too_loud/4.jpg",
+            "too_loud/5.jpg",
+            "too_loud/6.jpg",
+            "too_loud/7.jpg", 
+            "too_loud/8.jpg", 
+            "too_loud/9.jpg", 
+            "too_loud/10.jpg",
+            "too_loud/11.jpg", 
+            "too_loud/12.jpg",
             // More images
         ]
     },
-    "ludlow": {
-        archive: false,
-        title: "Ludlow Event",
-        id: "ludlow",
-        flyerSrc: "flyers/ludlow_house.jpeg",
-        images: [
-            "gallery/ludlow/000014720010.jpg",
-            "gallery/ludlow/000014720016.jpg",
-            // More images
-        ]
-    },
-    "ludlow": {
+    "open_the_garage": {
         archive: true,
-        title: "Ludlow Event",
-        id: "ludlow",
-        flyerSrc: "flyers/ludlow_house.jpeg",
+        title: "Open the Garage!",
+        id: "open_the_garage",
+        flyerSrc: "flyers/lume_green.jpeg",
         images: [
-            "gallery/ludlow/000014720010.jpg",
-            "gallery/ludlow/000014720016.jpg",
-            // More images
+            "open_garage/1.jpg",
+            "open_garage/2.jpg",
+            "open_garage/3.jpg",
+            "open_garage/4.jpg",
+            "open_garage/5.jpg",
+            "open_garage/6.jpg",
+            "open_garage/7.jpg", 
+            "open_garage/8.jpg", 
+            "open_garage/9.jpg", 
+            "open_garage/10.jpg", 
+        ]
+    },
+    "convocation": {
+        archive: true,
+        title: "CONVOCATION",
+        id: "convocation",
+        flyerSrc: "flyers/kind_regards.jpeg",
+        images: [
+            "convocation/1.jpg",
+            "convocation/2.jpg",
+            "convocation/3.jpg",
+            "convocation/4.jpg",
+            "convocation/5.jpg",
+            "convocation/6.jpg",
+            "convocation/7.jpg",
         ]
     }
-
-    "session": {
-        archive: true,
-        title: "Saturday Session Event",
-        id: "session",
-        flyerSrc: "flyers/ludlow_house.jpeg",
-        images: [
-            "gallery/ludlow/000014720010.jpg",
-            "gallery/ludlow/000014720016.jpg",
-            // More images
-        ]
-    }
-    // More events
 };
