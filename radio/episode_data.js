a9ec545081330849ee62880aa5dfa71d5af7d73c
William O'Brien <wgo2104@columbia.edu>
2024-01-22 22:32:54 -0500

    some things didnt push

---
+++ radio/episode_data.js
@@ -0,0 +1,59 @@
+export const episodeData = {
+ 
+    "1": {
+        name: "EP8",
+        cover: "covers/8.jpg",
+        link: "https://www.youtube.com/watch?v=Y9c-ErUEvz8",
+        caption: "",
+    },
+
+    "2": {
+        name: "EP7",
+        cover: "covers/7.jpg",
+        link: "https://www.youtube.com/watch?v=U5czzBivOD4&t=4s",
+        caption: "",
+    },
+
+    "3": {
+        name: "EP6",
+        cover: "covers/6.jpg",
+        link: "https://soundcloud.com/dennisfree/lvl-iii-radio-ep-6-live-from-jeans-lafayette?in=levelthreeradio/sets/level-iii-radio-on-halfmoon-bk&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+        caption: "",
+    },
+
+    "4": {
+        name: "EP5",
+        cover: "covers/5.jpg",
+        link: "https://soundcloud.com/dennisfree/lvl-iii-radio-episode-5-elsewhere-practice-session?in=levelthreeradio/sets/level-iii-radio-on-halfmoon-bk&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+        caption: "",
+    },
+
+    "5": {
+        name: "EP4",
+        cover: "covers/4.jpg",
+        link: "https://soundcloud.com/levelthreeradio/level-iii-radio-ep-4-on-halfmoon-bk?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+        caption: "",
+    },
+
+    "6": {
+        name: "EP3",
+        cover: "covers/3.jpg",
+        link: "https://soundcloud.com/halfmoonbk/71523-level-iii-radio-w-dennis-free?in=levelthreeradio/sets/level-iii-radio-on-halfmoon-bk&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+        caption: "",
+    },
+
+    "7": {
+        name: "EP2",
+        cover: "covers/2.jpg",
+        link: "https://soundcloud.com/levelthreeradio/level-iii-radio-episode-2-on-halfmoon-bk?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+        caption: "",
+    },
+
+    "8": {
+        name: "EP1",
+        cover: "covers/1.jpg",
+        link: "https://soundcloud.com/levelthreeradio/level-iii-radio-1?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+        caption: "",
+    },
+
+};
