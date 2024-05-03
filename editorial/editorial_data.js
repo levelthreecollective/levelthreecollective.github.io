c99f1e2747f377d982316a59e2845495bccb9a1a
William O'Brien <wgo2104@columbia.edu>
2024-05-02 21:03:58 -0400

    editorial inital

---
+++ editorial/editorial_data.js
@@ -0,0 +1,26 @@
+export const editorialData = {
+    "zano" : {
+        name: "Zano",
+        cover: "zano/Zano1.jpg",
+        content: [
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            , "zano/Zano6.jpg"
+            , 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            , "zano/zano10.jpg"
+            , 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        ]
+    },
+
+    "niara" : {
+        name: "Niara",
+        cover: "niara/Niara2.jpg",
+        content: [
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            , "niara/Niara3.jpg"
+            , 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            , "niara/Niara5.jpg"
+            , 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        ]
+    }
+
+};
---
+++ editorial/profile/load_editorial.js
@@ -0,0 +1,87 @@
+import { editorialData } from '../editorial_data.js';
+
+function loadEpisodes(editorialData) {
+    const episodeList = document.querySelector('.episode-list');
+    console.log(window.innerWidth < 1000);
+    console.log(window.innerWidth);
+    if (window.innerWidth < 1000) {
+        const col = document.createElement('episodeColumn');
+        Object.values(editorialData).forEach((episode,index) => {
+            const episodeItem = createEpisodeItem(episode);
+            // Append the item to the gallery container
+            col.appendChild(episodeItem);
+            episodeList.appendChild(col);
+     
+        });
+    } else if (window.innerWidth >= 1000) {
+        const column1 = document.createElement('episodeColumn1');
+        const column2 = document.createElement('episodeColumn2');
+
+        Object.values(editorialData).forEach((episode,index) => {
+            const episodeItem = createEpisodeItem(episode);
+            // Append the item to the gallery container
+
+            if (index % 2 === 0) {
+                column1.appendChild(episodeItem);
+            } else {
+                column2.appendChild(episodeItem);
+            }
+            episodeList.appendChild(column1);
+            episodeList.appendChild(column2);
+        });
+    }
+}
+
+function windowRefresh() {
+    let wasAbove1000 = window.innerWidth >= 1000;
+    window.addEventListener('resize', function() {
+
+        const isAbove1000 = window.innerWidth >= 1000;
+
+        if (isAbove1000 !== wasAbove1000) {
+            window.location.reload();
+        }
+
+        wasAbove1000 = isAbove1000;
+
+    });
+}
+
+function createEpisodeItem(episode) {
+    // Create a container for each image and caption
+    const episodeItem = document.createElement('div');
+    episodeItem.className = 'episode-item';
+
+    // Create and append the image
+    const episodeLink = document.createElement('a');
+    episodeLink.href = episode.link;
+
+    const img = document.createElement('img');
+    img.src = episode.cover;
+    img.className = 'episode-image';
+    episodeLink.appendChild(img);
+    episodeItem.appendChild(episodeLink);
+    
+    // Create and append Name
+    const episodeLink2 = document.createElement('a');
+    episodeLink2.className = 'episode-link';
+    episodeLink2.href = episode.link;
+
+    const nameh2 = document.createElement('h2');
+    nameh2.className = 'episode-name';
+    nameh2.textContent = episode.name;
+    episodeLink2.appendChild(nameh2);
+    episodeItem.appendChild(episodeLink2);
+
+    // Create and append the caption
+    const captionDiv = document.createElement('div');
+    captionDiv.className = 'episode-bio';
+    captionDiv.textContent = episode.caption;
+    episodeItem.appendChild(captionDiv);
+
+    return episodeItem;
+}
+
+// Call this function when you want to load the gallery
+loadEpisodes(editorialData);
+windowRefresh();
