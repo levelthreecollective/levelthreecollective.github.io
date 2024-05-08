51910af4ab08b946bde28cef67ff4d72b801c3b8
William O'Brien <wgo2104@columbia.edu>
2024-05-08 12:33:55 -0400

    events fix

--- events/display_flyers.js
+++ events/display_flyers.js
@@ -4,7 +4,8 @@ function loadFlyers() {
     const upcoming_gallery = document.querySelector('.flyer-gallery');
     const h2u = document.createElement('h2');
     h2u.textContent = 'UPCOMING';
-    h2u.style.border = '2px solid #f9f9f9';
+    h2u.style.borderTop = '5px solid #f9f9f9';
+    h2u.style.borderBottom = '5px solid #f9f9f9';
     upcoming_gallery.appendChild(h2u);
     let upcoming_count=0;
     Object.keys(eventData).forEach(key => {
@@ -40,7 +41,8 @@ function loadFlyers() {
     const archive_gallery = document.querySelector('.flyer-gallery');
     const h2a = document.createElement('h2');
     h2a.textContent = 'ARCHIVE';
-    h2a.style.border = '2px solid #f9f9f9';
+    h2a.style.borderTop = '5px solid #f9f9f9';
+    h2a.style.borderBottom = '5px solid #f9f9f9';
     archive_gallery.appendChild(h2a);
     Object.keys(eventData).forEach(key => {
         const event = eventData[key];
@@ -53,8 +55,8 @@ function loadFlyers() {
             const img = document.createElement('img');
             img.src = event.flyerSrc;
             img.alt = event.title;
-            img.style.border = '4px solid #f9f9f9';
-            img.style.borderRadius = '10px';
+            img.style.border = '4px solid #006636ba';
+            img.style.borderRadius = '4px';
 
             // Append the image to the link, and the link to the container
             a.appendChild(img);
